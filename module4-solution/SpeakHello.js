cd830756dbdacf1abb4c3a6f554b636feadc5fd9
ldgjustjazzzy <77585514+ldgjustjazzzy@users.noreply.github.com>
2022-05-15 14:23:33 -0400

    update

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -2,7 +2,7 @@
 // See Lecture 52, part 2
 (function(window) {
     var helloSpeaker = {};
-    //helloSpeaker.name = names[i];
+    helloSpeaker.name = names[i];
     var speakWord = "Hello";
     helloSpeaker.speak = function() {
         console.log(speakWord + " " + name);
